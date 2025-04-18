aa0632d3de1d2ec53641bb2578e4f000accfb96f
stvncode <steven.travers20@gmail.com>
2025-04-18 08:57:41 +0200

    updates

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,105 +1,26 @@
-import { cva } from "class-variance-authority"
+import { cn } from "@/lib/utils"
 import * as React from "react"
-import { cn } from "../../lib/utils"
 
-const inputVariants = cva(
-  "border-border bg-background ring-offset-background placeholder:text-xs placeholder:text-secondary placeholder:font-light flex w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 disabled:placeholder:text-hover-icon",
-  {
-    variants: {
-      size: {
-        default: "h-8",
-        xs: "h-6",
-        sm: "h-7",
-        md: "h-8",
-        lg: "h-[36px]",
-        xl: "h-10",
-      },
-      variant: {
-        default: "focus:border-transparent focus:shadow-focus",
-        ghost: "border-0 p-0 outline-none ring-0",
-      },
-    },
-    defaultVariants: {
-      size: "md",
-      variant: "default",
-    },
-  }
-)
-
-export type InputProps = {
-  icon?: React.ReactNode
-  endIcon?: React.ReactNode
-  endText?: string
-  rounded?: boolean
-  size?: "xs" | "sm" | "md" | "lg" | "xl"
-  variant?: "default" | "ghost"
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  (
-    {
-      className,
-      type,
-      endText,
-      icon,
-      size,
-      variant,
-      rounded,
-      endIcon,
-      ...props
-    },
-    ref
-  ) => {
-    const textAfterWidth = endText ? `${endText.toString().length}ch` : "auto"
+  ({ className, type, ...props }, ref) => {
+    React.useEffect(() => {
+      if (!props.disabled) document.getElementById("chat-input")?.focus()
+    }, [props.disabled])
 
     return (
-      <div className="relative">
-        {rounded ? (
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `
-            input:not([type='checkbox']):not([type='radio']),
-            textarea {
-              border-radius: 0.375em !important;
-            }
-            `,
-            }}
-          ></style>
-        ) : null}
-        <div className="flex items-center">
-          {icon ? <div className="absolute ml-2.5">{icon}</div> : null}
-          <input
-            className={cn(inputVariants({ size, variant }), className)}
-            ref={ref}
-            type={type}
-            {...props}
-            style={{
-              paddingLeft: icon ? "2.1rem" : undefined,
-              paddingRight:
-                endText || endIcon
-                  ? `calc(${textAfterWidth} + 2rem)`
-                  : undefined,
-            }}
-          />
-          {endText ? (
-            <div
-              className="text-secondary pointer-events-none absolute bottom-0 right-0 top-0 flex items-center px-2 text-xs"
-              style={{
-                overflow: "hidden",
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {endText}
-            </div>
-          ) : null}
-          {endIcon ? (
-            <div className="pointer-events-none absolute bottom-0 right-0 top-0 flex items-center pr-2">
-              {endIcon}
-            </div>
-          ) : null}
-        </div>
-      </div>
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus:border focus:border-primary disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        id="chat-input"
+        ref={ref}
+        autoFocus={true}
+        {...props}
+      />
     )
   }
 )
--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -7,11 +7,15 @@ const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> & {
     icon?: React.ReactNode
+    size?: "sm" | "md" | "lg"
   }
->(({ className, icon, ...props }, ref) => (
+>(({ className, icon, size, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      size === "sm" && "h-4 w-8",
+      (!size || size === "md") && "h-5 w-9",
+      size === "lg" && "h-6 w-10",
+      "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
       className
     )}
     {...props}
@@ -19,7 +23,10 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
+        size === "sm" && "h-3 w-3",
+        (!size || size === "md") && "h-4 w-4",
+        size === "lg" && "h-5 w-5",
+        "pointer-events-none block rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
       )}
     >
       {icon}
